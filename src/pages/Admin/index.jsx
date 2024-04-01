68d4d14dfb8b1ecc8396c43f67902febacaedcb1
DevMessias <messiasbento944@gmail.com>
2024-04-01 15:51:58 -0300

    Feat: SignOut

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,45 @@
-import './admin.css'
+import { useState } from 'react';
+import './admin.css';
+
+import { auth } from '../../firebaseConnection';
+import { signOut } from 'firebase/auth';
 
 function Admin(){
+    const [tarefaInput, setTarefaInput] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault();
+
+        alert("Click")
+    }
+
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
     return(
-        <div>
-            <h1>Admin</h1>
+        <div className='admin-container' >
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister} >
+                <textarea
+                    placeholder='Digite sua tarefa...'
+                    value={tarefaInput}
+                    onChange={(e) => setTarefaInput(e.target.value)}
+                />
+
+                <button className='btn-register' type='submit' >Registrar tarefa</button>
+            </form>
+
+            <article className='list' >
+                <p>Estudar javascrit hoje</p>
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete' >Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' onClick={handleLogout} >Sair</button>
         </div>
     )
 }
